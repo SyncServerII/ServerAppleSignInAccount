f9949f6f27bc4a7466f513f04c6ac7638855f4f2
Christopher Prince <chris@SpasticMuffin.biz>
2020-12-22 21:17:54 -0700

    Fix NotificationResponse

--- Sources/ServerAppleSignInAccount/NotificationResponse.swift
+++ Sources/ServerAppleSignInAccount/NotificationResponse.swift
@@ -3,7 +3,14 @@ import Foundation
 import ServerShared
 
 // Apple doesn't need this; just satisfying SyncServerII.
-public class NotificationResponse: ResponseMessage {
-    public init() {}
-    public var responseType:ResponseType = .json
+public class NotificationResponse : ResponseMessage {
+    required public init() {}
+
+    public var responseType: ResponseType {
+        return .json
+    }
+
+    public static func decode(_ dictionary: [String: Any]) throws -> NotificationResponse {
+        return NotificationResponse()
+    }
 }
